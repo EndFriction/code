10e9d443c67a50ae3cfc42b291d22c842b407543
Amarjeet Kumar <78524159+Me-amarJEEt@users.noreply.github.com>
2023-10-04 00:38:02 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    const dropdown = document.querySelector(".dropdown");
+    
+    burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    dropdown.classList.add("dropend");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
